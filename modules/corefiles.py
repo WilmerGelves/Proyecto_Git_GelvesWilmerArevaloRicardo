0484aca31c6031914ab2d7b76e1e290f0f2ac3f4
RicardoArevaloB <160280340+RicardoArevaloB@users.noreply.github.com>
2024-08-16 16:02:46 -0500

    feat: :sparkles: modulo corefiles
    creamos  en el arcivo corefiles las funciones que permitaran agragar nuevos datos que seran guardados en un arcivo .json

---
+++ modules/corefiles.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+MY_DATABASE = 'data/'
+
+def NewFile(*param):
+    with open(MY_DATABASE,"w") as wf:
+        json.dump(param[0],wf,indent=4)
+
+def UpdateFile(*param):
+    with open(MY_DATABASE,'w') as fw:
+        json.dump(param[0],fw,indent=4)
+
+def AddData(*param):
+    data = list(param)
+    with open(MY_DATABASE,"r+") as rwf:
+        data_file=json.load(rwf)
+        if (len(param) > 1):
+            data_file[data[0]].update({data[1]:data[2]})
+        else:
+            data_file.update({param[0]})
+        # data_file[llavePrincipal].update({codigo:info})
+        rwf.seek(0)
+        json.dump(data_file,rwf,indent=4)
+
